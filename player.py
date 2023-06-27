396263d9623fa332ae9b490ad20fdd5cdc4e1b9a
Codeit.Michael <michaelmaranan030@gmail.com>
2023-06-28 01:38:48 +0800

    add player animations

--- player.py
+++ player.py
@@ -1,10 +1,13 @@
 import pygame
+from support import import_folder
 
 class Player(pygame.sprite.Sprite):
 	def __init__(self, pos):
 		super().__init__()
-		self.image = pygame.Surface((25, 40))
-		self.image.fill(pygame.Color("indigo"))
+		self.import_character_assets()
+		self.frame_index = 0
+		self.animation_speed = 0.15
+		self.image = self.animations["idle"][self.frame_index]
 		self.rect = self.image.get_rect(topleft = pos)
 
 		# player movement
@@ -13,14 +16,49 @@ class Player(pygame.sprite.Sprite):
 		self.gravity = 1
 		self.jump_speed = -16
 
+		# player status
+		self.status = "idle"
+
+	def import_character_assets(self):
+		character_path = "img/pete/"
+		self.animations = {
+			"idle": [],
+			"walk": [],
+			"jump": [],
+			"fall": []
+		}
+		for animation in self.animations.keys():
+			full_path = character_path + animation
+			self.animations[animation] = import_folder(full_path)
+
+	def animate(self):
+		animation = self.animations[self.status]
+		self.frame_index += self.animation_speed
+		if self.frame_index >= len(animation):
+			self.frame_index = 0
+		self.image = animation[int(self.frame_index)]
+
 	def get_input(self, clicked_key):
 		if clicked_key != False:
 			if clicked_key == "right":
 				self.direction.x = 1
+				self.animate()
 			elif clicked_key == "left":
 				self.direction.x = -1
+				self.animate()
 		else:
 			self.direction.x = 0
+			self.animate()
+
+	def get_status(self):
+		if self.direction.y < 0:
+			self.status = "jump"
+		elif self.direction.y > 0:
+			self.status = "fall"
+		elif self.direction.x != 0:
+			self.status = "walk"
+		else:
+			self.status = "idle"
 
 	def apply_gravity(self):
 		self.direction.y += self.gravity
@@ -30,7 +68,9 @@ class Player(pygame.sprite.Sprite):
 		self.direction.y = self.jump_speed
 
 	def update(self, clicked_key):
+		self.get_status()
 		if clicked_key == "space":
 			self.jump()
 		else:
 			self.get_input(clicked_key)
+
